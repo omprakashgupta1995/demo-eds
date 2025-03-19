d1ccb1e11c4692852fcc0a00664af880ae484b91
Om Gupta <om.gupta@teknopoint.in>
2025-03-19 16:49:51 +0530

    carousel block created

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,14 @@
+import Swiper1 from './swiper-bundle.min.js';
+
+export default function decorate(block) {
+  block.classList.add('swiper');
+  const swiperWrapper = document.createElement('div');
+  swiperWrapper.classList.add('swiper-wrapper');
+  Array.from(block.children).forEach((row) => {
+    row.classList.add('swiper-slide');
+    swiperWrapper.append(row);
+  });
+  block.append(swiperWrapper);
+
+  Swiper1(block);
+}
