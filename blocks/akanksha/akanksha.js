7b304c9f6f41b6c90fbcf899debe87ddec686d9b
sakpal-A <akanksha.sakpal@deptagency.com>
2025-04-02 19:28:24 +0530

    initial commit

---
+++ blocks/akanksha/akanksha.js
@@ -0,0 +1,15 @@
+export default function decorate(block){
+    let outerdiv = block.children;
+    console.log(outerdiv);
+
+    Array.from(outerdiv).forEach((row, rowIndex) => {
+        let rowClass = `row-${rowIndex + 1}`;
+        row.classList.add(rowClass, "rowcls");
+
+        let colClass = row.children;
+        Array.from(colClass).forEach((col, colIndex) => {
+            let colClasses = `${rowClass}-col-${colIndex + 1}`;
+            col.classList.add(colClasses);
+        })
+    })
+}
