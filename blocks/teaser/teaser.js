73bf5363671eb1a909fa2a0f154ba2b0ad2bf1ac
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-03 11:59:11 +0530

    Add Position Property in Teaser

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -2,12 +2,14 @@ export default function decorater(teaser) {
   const containers = teaser.children[0];
 
   // console.log(containers)
-  containers.classList.add("flex-container");
+  containers.classList.add(`flex-container`);
+  containers.classList.add(`post`);
+
 
   const containerAll = Array.from(containers.children);
   containerAll.forEach((divs, divsIndex) => {
     divs.classList.add(`box-${divsIndex+1}`);
-    
+
     console.log(divs);
   });
 }
