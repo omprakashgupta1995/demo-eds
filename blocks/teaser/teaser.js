e2b8ff76e058b24be1c84860bdb9e6ff92bb1010
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-04-03 11:16:39 +0530

    Completed given Task

---
+++ blocks/teaser/teaser.js
@@ -0,0 +1,13 @@
+export default function decorater(teaser) {
+  const containers = teaser.children[0];
+
+  // console.log(containers)
+  containers.classList.add("flex-container");
+
+  const containerAll = Array.from(containers.children);
+  containerAll.forEach((divs, divsIndex) => {
+    divs.classList.add(`box-${divsIndex+1}`);
+
+    console.log(divs);
+  });
+}
